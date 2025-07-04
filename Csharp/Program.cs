23546464b91d7b4d6d569911f01727bc0abac11c
byabbbbi <thesiminmeat@gmail.com>
2025-07-04 15:50:41 +0900

    나머지미션해결

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -2,6 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("hello 김연xccxvvcxccccvxcvxcvdffd화");
+        //테스트
+        Console.WriteLine(Solution0704(1342343420, 5342));
+    }
+
+    public static int Solution0704(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 % num2;
+        return answer;
     }
 }
